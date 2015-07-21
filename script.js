07be870bb3900a5ed9855f3ed62dd6521740428e
David Nascimento <davidpn.11@gmail.com>
2015-07-21 15:15:45 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,59 @@
+
+
+var playerName;
+var botaoInicia = document.querySelector('#iniciar-jogo');
+var janelaInicial = document.getElementById('game-begin');
+
+
+botaoInicia.addEventListener('click', function (event) {
+	
+	startBaralho();
+    playerName = document.getElementById('player-name').value;        
+    janelaInicial.style.visibility = "hidden";
+
+    var elements = document.getElementsByClassName('hidden-class');
+	for (var i in elements) {
+  		if (elements.hasOwnProperty(i)) {
+    		elements[i].className = 'show-class';
+  		}  	
+	}
+});
+
+
+var nipe = ['copa','paus','ouros','espadas'];
+var tipo = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+
+function carta(nipe, tipo, valor){
+	this.nipe = nipe;
+	this.tipo = tipo;
+	this.valor = valor;
+}
+
+
+var baralho = [];
+var count=0;
+
+
+function startBaralho(){
+	baralho = [];	
+	var temp;
+	for(var i = 0; i < nipe.length; i++){
+		for(var j = 0; j < tipo.length; j++){
+			
+			if(j > 9 ){
+				temp = new carta(nipe[i], tipo[j], 10);
+			
+			} else{
+				temp = new carta(nipe[i], tipo[j], j + 1);
+			}
+
+			baralho.push(temp);
+		}
+	}
+}
+	
+
+
+
+
+
